22a85c25796350fbecbd0114ede7fb75f12beed3
Egor Konovalov <73017521+egorkonovalov@users.noreply.github.com>
2023-07-07 18:46:07 +0400

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import MusicLibrary from 'music-library-service';
+
+	let songUrl = '';
+</script>
+
+<form action="submit">
+	<input type="text" placeholder="Insert song url" bind:value={songUrl} />
+	<button type="submit">Get the song</button>
+</form>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
